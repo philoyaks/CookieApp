fb695a2e4828928791cfd319c7257e62a4a2d833
Oyaleke Philip <oyaleke.philip@gmail.com>
2020-08-20 19:46:14 +0100

    change

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cokies',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
